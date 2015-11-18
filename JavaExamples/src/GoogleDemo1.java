003cd841cb98e99ae1a37c67be939b63e1ca9e44
Garrett Benoit <Garrett_Benoit@baylor.edu>
2015-11-17 22:38:41 -0600

    first google demo

---
+++ JavaExamples/src/GoogleDemo1.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class GoogleDemo1 {
+
+	public static void main(String [] args) {
+		int [] A = {-1,3,-4,5,1,-6,2,1};
+		System.out.println(solution(A));
+	}
+	
+	public static int solution(int[] A) {
+		long totalSum = 0;
+		long [] leftSums = new long [A.length];
+		// find sums as we move right and total sum
+		for(int i = 0; i < A.length; i++) {
+			totalSum += A[i];
+			leftSums[i] = totalSum;
+		}
+		for(int p = 0; p < A.length; p++) {
+			long value = A[p];
+			long leftSum = p == 0 ? 0 : leftSums[p-1];
+			long rightSum = p+1 >= A.length ? 0 : ((totalSum - value) - leftSum);
+			if(leftSum == rightSum) return p;
+		}
+		return -1;
+	}
+}
