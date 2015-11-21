c0294eb51c32c6872df71cb51733c0668cf80e00
Garrett Benoit <Garrett_Benoit@baylor.edu>
2015-11-20 20:34:24 -0600

    O(n) time to solve if you solve it backwards.

--- JavaExamples/src/examples/google/GoogleSample2.java
+++ JavaExamples/src/examples/google/GoogleSample2.java
@@ -6,7 +6,7 @@ public class GoogleSample2 {
 		String S = "dir1\n"
 				+ " dir11\n"
 				+ " dir12\n"
-				+ "  picture.jpeg\n"
+				+ "  picturepicturepicturepicturepicture.jpeg\n"
 				+ "  dir121\n"
 				+ "  file1.txt\n"
 				+ "  dir3\n"
@@ -15,10 +15,40 @@ public class GoogleSample2 {
 				+ "    longpicture.png\n"
 				+ "  dir4\n"
 				+ "dir2\n"
-				+ " file2.gif\n";
-		System.out.println(solution2(S));
+				+ " test.gif\n";
+		System.out.println(solution3(S));
 	}
 	
+	public static int solution3(String S) {
+		int max = 0;
+		String [] parts = S.split("\n");
+		max = solution3Recursive(parts, parts.length, parts.length-1, "");
+		return max;
+	}
+	
+	public static int solution3Recursive(String [] parts, int lastLevel, int i, String path) {
+		if(i == -1) return 0;
+		String part = parts[i];
+		int level = part.lastIndexOf(' ') + 1;
+		part = part.replaceAll("\\s+", "");
+		int max = 0;
+		if(level > lastLevel) {
+			path = "/" + part;
+		} else if(level < lastLevel) {
+			path = "/" + part + path;
+			
+		} else if((part.contains(".jpeg") ||
+				part.contains(".png") ||
+				part.contains(".gif")) &&
+				part.length()+1 > path.length()) {
+			path = "/" + part;
+		}
+		System.out.println(path);
+		max = Math.max(path.length(), solution3Recursive(parts, level, i-1, path));
+		return max;
+	}
+	
+	// O(h*n) time complexity
 	public static int solution2(String S) {
 		int max = 0;
 		String [] parts = S.split("\n");
@@ -35,23 +65,32 @@ public class GoogleSample2 {
 		if(level > lastLevel) {
 			path += "/" + part;
 		} else if(level < lastLevel) {
-			return 0;
-		} else if(level == lastLevel) {
+			for(int dif = (lastLevel - level); dif > 0; dif--) {
+				path = path.substring(0, path.lastIndexOf('/'));
+			}
+			if(level == 0) {
+				path = "/" + part;
+			} else {
+				path = path.substring(0, path.lastIndexOf('/'));
+				path += "/" + part;	
+			}
+		} else {
 			path = path.substring(0, path.lastIndexOf('/'));
 			path += "/" + part;
 		}
+		System.out.println(path);
 		max = Math.max(max, solution2Recursive(parts, level, i+1, path));
-		if(path.contains(".jpeg") ||
+		if((path.contains(".jpeg") ||
 				path.contains(".png") ||
 				path.contains(".gif") &&
-				level >= lastLevel) {
-			System.out.println(path);
+				level >= lastLevel)) {
 			return Math.max(max, path.length());
 		}
 		return max;
 	}
 	
-	public static int solution(String S) {;
+	// O(h*n) time complexity
+	public static int solution1(String S) {;
 		String longestPath = "";
 		String currentPath = "";
 		String [] parts = S.split("\n");
@@ -78,9 +117,9 @@ public class GoogleSample2 {
 			}
 			lastLevel = level;
 			
-			if(currentPath.contains(".jpeg") ||
+			if((currentPath.contains(".jpeg") ||
 					currentPath.contains(".png") ||
-					currentPath.contains(".gif") &&
+					currentPath.contains(".gif")) &&
 					currentPath.length() > longestPath.length()) {
 				longestPath = currentPath;
 			}
